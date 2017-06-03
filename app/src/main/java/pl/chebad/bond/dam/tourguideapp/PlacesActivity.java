0e4c1f6fa092100139d672d5fd4335e6dd7193e8
Damian <chebad@interia.pl>
2017-06-03 19:14:36 +0200

    Added dependency for TabLayout. Added icons into TabLayout. Added new icons (for all DPI).

--- app/src/main/java/pl/chebad/bond/dam/tourguideapp/PlacesActivity.java
+++ app/src/main/java/pl/chebad/bond/dam/tourguideapp/PlacesActivity.java
@@ -1,12 +1,22 @@
 package pl.chebad.bond.dam.tourguideapp;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.NavUtils;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import pl.chebad.bond.dam.tourguideapp.Adapter.TourGuideFragmentAdapter;
 
 public class PlacesActivity extends AppCompatActivity {
+    private int[] tabIcons = {
+            R.drawable.ic_fun,
+            R.drawable.ic_culture,
+            R.drawable.ic_health_activity,
+            R.drawable.ic_restaurant,
+            R.drawable.ic_school
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +24,26 @@ public class PlacesActivity extends AppCompatActivity {
         setContentView(R.layout.fragment_places);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         TourGuideFragmentAdapter fragmentAdapter = new TourGuideFragmentAdapter(getSupportFragmentManager());
 
+        getSupportActionBar().setElevation(0);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         viewPager.setAdapter(fragmentAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        for (int i = 0; i < tabIcons.length; i++) {
+            tabLayout.getTabAt(i).setIcon(tabIcons[i]);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
