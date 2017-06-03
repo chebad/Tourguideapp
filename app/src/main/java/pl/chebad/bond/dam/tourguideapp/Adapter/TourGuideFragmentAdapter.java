067cfb2ab86a5a16e571f151ff1d65fc4f001eae
Damian <chebad@interia.pl>
2017-06-03 15:43:45 +0200

    Created ground(fragments) for all places.

--- app/src/main/java/pl/chebad/bond/dam/tourguideapp/Adapter/TourGuideFragmentAdapter.java
+++ app/src/main/java/pl/chebad/bond/dam/tourguideapp/Adapter/TourGuideFragmentAdapter.java
@@ -4,7 +4,11 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import pl.chebad.bond.dam.tourguideapp.Fragments.CultureFragment;
+import pl.chebad.bond.dam.tourguideapp.Fragments.EducationFragment;
 import pl.chebad.bond.dam.tourguideapp.Fragments.FunFragment;
+import pl.chebad.bond.dam.tourguideapp.Fragments.HealthActivitiesFragment;
+import pl.chebad.bond.dam.tourguideapp.Fragments.RestaurantFragment;
 
 /**
  * Created by Damian Bondaruk on 2017-06-03.
@@ -17,11 +21,34 @@ public class TourGuideFragmentAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        return new FunFragment();
+        Fragment fragment = null;
+        switch (position) {
+            case 0: {
+                fragment = new FunFragment();
+                break;
+            }
+            case 1: {
+                fragment = new CultureFragment();
+                break;
+            }
+            case 2: {
+                fragment = new HealthActivitiesFragment();
+                break;
+            }
+            case 3: {
+                fragment = new EducationFragment();
+                break;
+            }
+            case 4: {
+                fragment = new RestaurantFragment();
+                break;
+            }
+        }
+        return fragment;
     }
 
     @Override
     public int getCount() {
-        return 1;
+        return 5;
     }
 }
---
+++ app/src/main/java/pl/chebad/bond/dam/tourguideapp/Fragments/HealthActivitiesFragment.java
@@ -0,0 +1,64 @@
+package pl.chebad.bond.dam.tourguideapp.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import pl.chebad.bond.dam.tourguideapp.Adapter.Adapter;
+import pl.chebad.bond.dam.tourguideapp.Model.Place;
+import pl.chebad.bond.dam.tourguideapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HealthActivitiesFragment extends Fragment {
+
+
+    public HealthActivitiesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.list_places, container, false);
+
+        ArrayList<Place> places = new ArrayList<>();
+
+        places.add(new Place("test test test", "test test test test test test test test test test test test " +
+                "test test test test test test test test test test test test test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+
+        Adapter adapter = new Adapter(getActivity(), places);
+        ListView listView = (ListView) rootView.findViewById(R.id.places_list_view);
+        listView.setAdapter(adapter);
+
+        return rootView;
+    }
+}
