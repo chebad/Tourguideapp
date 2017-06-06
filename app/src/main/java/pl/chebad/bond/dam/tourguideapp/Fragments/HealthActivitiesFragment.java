4a34fb6bb73cf525df71e12c9869a947c6fa8442
Damian <chebad@interia.pl>
2017-06-06 17:17:52 +0200

    Added images into health activities with all descriptions.

--- app/src/main/java/pl/chebad/bond/dam/tourguideapp/Fragments/HealthActivitiesFragment.java
+++ app/src/main/java/pl/chebad/bond/dam/tourguideapp/Fragments/HealthActivitiesFragment.java
@@ -18,12 +18,16 @@ import pl.chebad.bond.dam.tourguideapp.R;
  * A simple {@link Fragment} subclass.
  */
 public class HealthActivitiesFragment extends Fragment {
-
+    private int[] tabHealthActivityPlaces = {
+            R.drawable.heath_national_stadium,
+            R.drawable.heath_orlen_marathon,
+            R.drawable.heath_vetrurilo,
+            R.drawable.heath_wodny_park
+    };
 
     public HealthActivitiesFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,28 +36,10 @@ public class HealthActivitiesFragment extends Fragment {
 
         ArrayList<Place> places = new ArrayList<>();
 
-        places.add(new Place("test test test", "test test test test test test test test test test test test " +
-                "test test test test test test test test test test test test test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
-        places.add(new Place("test test test", "test test test test test test test test test test test test", R.mipmap.ic_launcher_round));
+        places.add(new Place(getString(R.string.health_national_stadium_tittle), getString(R.string.health_national_stadium_description), tabHealthActivityPlaces[0]));
+        places.add(new Place(getString(R.string.health_orlen_marathon_tittle), getString(R.string.health_orlen_marathon_description), tabHealthActivityPlaces[1]));
+        places.add(new Place(getString(R.string.health_veturilo_tittle), getString(R.string.health_verturilo_bikes_description), tabHealthActivityPlaces[2]));
+        places.add(new Place(getString(R.string.health_wodny_park_tittle), getString(R.string.health_wodny_park_description), tabHealthActivityPlaces[3]));
 
         Adapter adapter = new Adapter(getActivity(), places);
         ListView listView = (ListView) rootView.findViewById(R.id.places_list_view);
--- app/src/main/java/pl/chebad/bond/dam/tourguideapp/Fragments/RestaurantFragment.java
+++ app/src/main/java/pl/chebad/bond/dam/tourguideapp/Fragments/RestaurantFragment.java
@@ -18,6 +18,13 @@ import pl.chebad.bond.dam.tourguideapp.R;
  * A simple {@link Fragment} subclass.
  */
 public class RestaurantFragment extends Fragment {
+    private int[] tabImageOfRestaurantsPlaces = {
+            R.drawable.rest_amber_room,
+            R.drawable.rest_rozana,
+            R.drawable.rest_platter,
+            R.drawable.rest_amrit_oriental,
+    };
+
     public RestaurantFragment() {
         // Required empty public constructor
     }
@@ -29,10 +36,10 @@ public class RestaurantFragment extends Fragment {
 
         ArrayList<Place> places = new ArrayList<>();
 
-        places.add(new Place(getString(R.string.rest_amber_room_tittle), getString(R.string.rest_amber_room_description), R.drawable.rest_amber_room));
-        places.add(new Place(getString(R.string.rest_rozana_tittle), getString(R.string.rest_rozana_description), R.drawable.rest_rozana));
-        places.add(new Place(getString(R.string.rest_platter_tittle), getString(R.string.rest_platter_description), R.drawable.rest_platter));
-        places.add(new Place(getString(R.string.rest_amrit_oriental_tittle), getString(R.string.rest_amrit_oriental_description), R.drawable.rest_amrit_oriental));
+        places.add(new Place(getString(R.string.rest_amber_room_tittle), getString(R.string.rest_amber_room_description), tabImageOfRestaurantsPlaces[0]));
+        places.add(new Place(getString(R.string.rest_rozana_tittle), getString(R.string.rest_rozana_description), tabImageOfRestaurantsPlaces[1]));
+        places.add(new Place(getString(R.string.rest_platter_tittle), getString(R.string.rest_platter_description), tabImageOfRestaurantsPlaces[2]));
+        places.add(new Place(getString(R.string.rest_amrit_oriental_tittle), getString(R.string.rest_amrit_oriental_description), tabImageOfRestaurantsPlaces[3]));
 
         Adapter adapter = new Adapter(getActivity(), places);
         ListView listView = (ListView) rootView.findViewById(R.id.places_list_view);
